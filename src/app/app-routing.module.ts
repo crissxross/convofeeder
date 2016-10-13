24d6072ff017cabfa211f2971b690e2409572874
Christine Wilks <crissxross@gmail.com>
2016-10-13 11:46:03 +0100

    add comments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,12 @@
+/**
+ * This file was created by angular-cli 1.0.0-beta.16
+ * but I'm not using this.
+ * I'm using app.routing.ts which I created before this cli update.
+ * Should I adapt this file instead?
+ * Or simply rename app.routing.ts to match the name of this file?
+ * Or leave as is?
+ */
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
